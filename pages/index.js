f9912b45dfac4e1d8cc0acbbe0301d8dccd71109
Bandit Silachai <mbandit001@yahoo.co.th>
2022-01-28 12:00:30 +0700

    edit text

--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ export default function Home() {
                 right={"-125px"}
                 top={"-15px"}
                 transform={"rotate(10deg)"}>
-                Starting at $15/mo
+                Starting now!
               </Text>
             </Box>
           </Stack>
